cea295b4c9279c14e1c3207afa7b015b25aa48a1
Noisycools <adityanh1604@gmail.com>
2021-01-28 08:01:09 +0700

    v1
    player movement

---
+++ Signals Finder/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float move_speed;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * move_speed, rb.velocity.y);
+    }
+}
