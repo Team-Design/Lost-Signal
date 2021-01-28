b62b73e97aa4cef2d8f2b90af6231c6d968f054b
Noisycools <adityanh1604@gmail.com>
2021-01-28 11:42:07 +0700

    v2
    signal bar and door script

---
+++ Signals Finder/Assets/Scripts/Door/DoorScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public GameObject sign;
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == "Player")
+        {
+            sign.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D target)
+    {
+        if (target.tag == "Player")
+        {
+            sign.SetActive(false);
+        }
+    }
+}
---
+++ Signals Finder/Assets/Scripts/Signal/SignalScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignalScript : MonoBehaviour
+{
+    private Slider signal_bar;
+
+    public float signal_lost_time = 0.05f;
+
+    private void Awake()
+    {
+        signal_bar = GetComponent<Slider>();
+    }
+
+    private void Update()
+    {
+        signal_bar.value -= signal_lost_time * Time.deltaTime;
+    }
+}
