7dc4ee9554dd59fbbda930919cfecfe67ab6aaed
sun-wise-man <67572372+sun-wise-man@users.noreply.github.com>
2021-01-29 19:21:38 +0700

    Revised Pause Menu
    - Added more option when paused
    -- Reset menu
    -- Setting menu
    -- Quit

--- Signals Finder/Assets/PauseMenu.cs
+++ Signals Finder/Assets/PauseMenu.cs
@@ -7,7 +7,10 @@ public class PauseMenu : MonoBehaviour
     public static bool gameIsPaused = false;
 
     public GameObject pausedMenuUI;
-    
+    public GameObject resetMenuUI;
+    public GameObject settingMenuUI;
+    public GameObject quitMenuUI;
+
 
     void Update()
     {
@@ -24,6 +27,10 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
+    //<SUMMARY>
+    //Pause Menu Controller
+    //<SUMMARY>
+
     public void Resume()
     {
         pausedMenuUI.SetActive(false);
@@ -36,5 +43,69 @@ public class PauseMenu : MonoBehaviour
         pausedMenuUI.SetActive(true);
         Time.timeScale = 0f;
         gameIsPaused = true;
+    }
+
+    //<SUMMARY>
+    //Reset Menu Controller
+    //<SUMMARY>
+    public void PauseReset()
+    {
+        pausedMenuUI.SetActive(false);
+        resetMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void ResumeReset()
+    {
+        pausedMenuUI.SetActive(true);
+        resetMenuUI.SetActive(false);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void ResetLevel()
+    {
+        //TODO Reset Level
+    }
+
+    //<SUMMARY>
+    //Setting Menu Controller
+    //<SUMMARY>
+    public void PauseSetting()
+    {
+        pausedMenuUI.SetActive(false);
+        settingMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+    public void ResumeSetting()
+    {
+        pausedMenuUI.SetActive(true);
+        settingMenuUI.SetActive(false);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    //<SUMMARY>
+    //Quit Menu Controller
+    //<SUMMARY>
+    public void PauseQuit()
+    {
+        pausedMenuUI.SetActive(false);
+        quitMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+    public void ResumeQuit()
+    {
+        pausedMenuUI.SetActive(true);
+        quitMenuUI.SetActive(false);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+    public void Quit()
+    {
+        //Application.Quit();
     }
 }
