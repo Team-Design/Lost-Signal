2c8f7fb9b79129e66b90192b4258ba64afdafe89
Noisycools <adityanh1604@gmail.com>
2021-01-29 19:19:19 +0700

    v5

---
+++ Signals Finder/Assets/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+    }
+
+        public void LevelChooser()
+    {
+        SceneManager.LoadScene("LevelChooser");
+    }
+
+    public void Title()
+    {
+        SceneManager.LoadScene("Title_Screen");
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
