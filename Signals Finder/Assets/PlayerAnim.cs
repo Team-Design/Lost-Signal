6abf08abf6f96f8fc7a16a05c3cc25851b20d72a
sun-wise-man <67572372+sun-wise-man@users.noreply.github.com>
2021-01-29 14:16:39 +0700

    Movement Animation
    - Added idle, running asset and animate it.
    - Using animator to control the state of the character.

---
+++ Signals Finder/Assets/PlayerAnim.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float direction = Input.GetAxisRaw("Horizontal");
+        if (direction != 0)
+        {
+            anim.SetBool("Running", true);
+        }
+        else
+        {
+            anim.SetBool("Running", false);
+        }            
+    }
+}
